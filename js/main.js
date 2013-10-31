bc814c03501570530760376f1b99d697f2a1b9d1
nickruiz <nick@fatcatonline.net>
2013-10-30 17:54:49 -0700

    page loading

--- js/main.js
+++ js/main.js
@@ -1,22 +1,44 @@
 function fadeIn(target) {
     target.style.opacity = 1;
     target.style.filter  = "alpha(opacity=100)";
-
 }
 
-function load() {
+function fadeOut(target) {
+    target.style.opacity = 0;
+    target.style.filter  = "alpha(opacity=0)";
+}
+
+function pageLoad() {
     var target = document.getElementById('container');
     fadeIn(target);
+    return false;
 }
 
-function toggleVideo(scroll) {
-    var counter = '-450px';
-    var target  = document.getElementById('landing');
+var scroll = true;
+
+function toggleVideo() {
+    var travel = '-450px';
+    var target = document.getElementById('landing');
 
     if(scroll){
         target.style.marginTop = 0;
     } else {
-        target.style.marginTop = counter;
+        target.style.marginTop = travel;
     }
+    scroll = !scroll;
     return false;
 }
+
+$(document).ready( function () {
+    $('.load').on('click', function (e) {
+        e.preventDefault();
+
+        var link   = $(this).attr('href');
+        var target = document.getElementById('container');
+
+        fadeOut(target);
+        $('#container').load(link, function() {
+            fadeIn(target);
+        });
+    });
+});
